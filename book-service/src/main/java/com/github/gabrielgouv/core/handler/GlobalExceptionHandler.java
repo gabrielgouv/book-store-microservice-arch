3f25b01d2ad1c320bf144a170692096c19fc0833
João Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2021-07-25 16:41:23 -0300

    feat: add global exception handler

---
+++ book-service/src/main/java/com/github/gabrielgouv/core/handler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.github.gabrielgouv.core.handler;
+
+import com.github.gabrielgouv.core.handler.dto.ErrorDTO;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Singleton;
+
+@Slf4j
+@Produces
+@Singleton
+@Requires(classes = { Throwable.class, ExceptionHandler.class })
+public class GlobalExceptionHandler implements ExceptionHandler<Throwable, HttpResponse<?>> {
+
+    @Override
+    public HttpResponse<?> handle(HttpRequest request, Throwable exception) {
+        log.error("GlobalExceptionHandler catch message={}, cause={}, path={}",
+                exception.getMessage(), exception.getCause(), request.getPath());
+        final ErrorDTO errorDTO = ErrorDTO.builder()
+                .code("GLOBAL")
+                .message("An error occurred")
+                .details(exception.getMessage())
+                .build();
+        return HttpResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDTO);
+    }
+
+}
---
+++ book-service/src/main/java/com/github/gabrielgouv/core/handler/dto/ErrorDTO.java
@@ -0,0 +1,23 @@
+package com.github.gabrielgouv.core.handler.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.github.gabrielgouv.core.util.DateUtil;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ErrorDTO {
+
+    private final String code;
+
+    private final String message;
+
+    private final String details;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateUtil.DEFAULT_DATE_TIME_FORMAT)
+    private final LocalDateTime at;
+
+}
