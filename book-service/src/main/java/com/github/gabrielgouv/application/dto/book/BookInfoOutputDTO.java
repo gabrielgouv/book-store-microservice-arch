b3f3f690e7b638f9c079180105f9b5269240549e
João Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2021-07-27 21:12:35 -0300

    feat: add BookInfoOutputDTO

---
+++ book-service/src/main/java/com/github/gabrielgouv/application/dto/book/BookInfoOutputDTO.java
@@ -0,0 +1,27 @@
+package com.github.gabrielgouv.application.dto.book;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Schema(name="BookInfoOutputDTO", description="Book info")
+public class BookInfoOutputDTO {
+
+    @Schema(description="Book ID")
+    private String id;
+
+    @Schema(description="Book title")
+    private String title;
+
+    @Schema(description="Book description")
+    private String description;
+
+    @Schema(description="Book price", type = "string")
+    private BigDecimal price;
+
+    @Schema(description="Book ISBN code")
+    private String isbn;
+
+}
